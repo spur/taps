dadfa89cbf83507c2e87fa6b649bddbaf29d9534
polco <paul@kkvesper.jp>
2015-12-07 12:58:45 +0900

    taps code

---
+++ src/taps.js
@@ -0,0 +1,54 @@
+let canTouch = false;
+if ('ontouchstart' in window) {
+  canTouch = true;
+}
+
+let currentTap = {
+  x: 0,
+  y: 0,
+  taps: [],
+  count: 0
+};
+
+function getTapWithTouch(e) {
+  let tapList = e.touches || [];
+  let tapCount = tapList.length;
+  let taps = [];
+
+  currentTap.count = tapCount;
+  currentTap.tapList = taps;
+
+  if (tapCount > 0) {
+    for (let i = 0; i < tapCount; i += 1) {
+      taps.push({ x: tapList[i].clientX, y: tapList[i].clientY });
+    }
+
+    currentTap.x = taps[0].x;
+    currentTap.y = taps[0].y;
+  }
+
+  return currentTap;
+}
+
+function getTapWithMouse(e) {
+  currentTap.x = e.clientX;
+  currentTap.y = e.clientY;
+  currentTap.count = e.type === 'mouseup' ? 0 : 1;
+  currentTap.taps = e.type === 'mouseup' ? [] : [{ x: currentTap.x, y: currentTap.y }];
+  return currentTap;
+}
+
+let getTap = canTouch ? getTapWithTouch : getTapWithMouse;
+
+let tapEvents = {
+  start: canTouch ? 'touchstart' : 'mousedown',
+  move: canTouch ? 'touchmove' : 'mousemove',
+  end: canTouch ? 'touchend' : 'mouseup'
+};
+
+export {
+  canTouch,
+  getTap,
+  currentTap,
+  tapEvents
+};
